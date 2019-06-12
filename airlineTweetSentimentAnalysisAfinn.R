3073137cb0d64c9cd3c6f6a551d5c13bc0111e1a
atofarides <antonist10@gmail.com>
2019-06-12 18:02:22 +0200

    Create airlineTweetSentimentAnalysisAfinn.R

---
+++ airlineTweetSentimentAnalysisAfinn.R
@@ -0,0 +1,74 @@
+# Performing analysis on airline tweets dataset using the Afinn lexicon
+
+# Loading necessary libraries
+library(readr)
+library(dplyr)
+library(tidyr)
+library(tidytext)
+library(stringr)
+library(ggplot2)
+
+# Reading tweets from csv. Also optionally elimintaing retweets by choosing distinct text entries. 
+# Removing all retweets would have also removed tweets whose original tweet has not 
+# been captured
+
+path <- file.path("C:", "Users", "anton", "Dropbox", "Twitter Data", "airlines.csv", fsep = "/")
+tweetsText <- read_csv(path, col_names = TRUE) 
+
+# Comment out the following line if you want to include retweets in analysis
+#tweetsText <- distinct(tweetsText,text,.keep_all = TRUE) # To be used in order to exclude retweets
+
+# Perform unnest tokens, adding the tweet ID on a separate column to keep track for further grouping 
+tweetsWords <- tweetsText %>%
+              mutate(.,tweetNumber = row_number()) %>%
+              unnest_tokens(., word, text)
+
+# Sentiment analysis using bing lexicon
+tweetsSentimentsAfinn <- tweetsWords %>%
+                    inner_join(get_sentiments("afinn"))
+
+# Create polarity matrix
+
+pol <- tweetsSentimentsAfinn %>%
+      group_by(airline,tweetNumber) %>%
+      summarise(polarity = sum(score)) %>%
+      ungroup()
+
+ggplot(pol, aes(x=polarity)) + geom_histogram(binwidth = 1, color="black",fill="white") + 
+  facet_wrap(facets = "airline", scales = "free") + 
+  ggsave(file.path("~", "Github","ieseDataSciTwitterProject", "airlinePolarityAfinn.pdf", fsep = "/"))
+
+# As we can see the airlines with the most positive comments, normalised to volume, are 
+# Positive: AlaskaAir, Delta, JetBlue, SouthWest, Allegiant
+# Negative: None explicitly
+
+# Create polarity thresholds to rank airlines. Polarity threshold levels were decided
+# by examining tweets qualitatively
+
+airlinePolarity <- pol %>%
+                    group_by(airline) %>%
+                    summarise(threshold1 = sum(polarity>=2)/sum(polarity<=-2),
+                              threshold2 = sum(polarity>=5)/sum(polarity<=-5),
+                              threshold3 = sum(polarity>=10)/sum(polarity<=-10)) %>%
+                    mutate(rankThr1 = rank(desc(threshold1)),
+                           rankThr2 = rank(desc(threshold2)),
+                           rankThr3 = rank(desc(threshold3)))
+
+airlinePolarity$avgRank <- 0
+for(i in 1:nrow(airlinePolarity)){
+  airlinePolarity$avgRank[i] <- round(mean(c(as.numeric(airlinePolarity[i,"rankThr1"]),
+         as.numeric(airlinePolarity[i,"rankThr2"]),
+         as.numeric(airlinePolarity[i,"rankThr3"]))))
+}
+
+# compare against ACSI
+acsiRank <- c(1,6,5,4,8,3,2,9,7)
+airlinePolarity <- mutate(airlinePolarity, acsi_rank = acsiRank, rank_diff = abs(avgRank-acsi_rank))
+mean(airlinePolarity$rank_diff) # 1.2 mean rank diff
+sd(airlinePolarity$rank_diff) # 1.8 standard deviation
+
+# write to csv
+write_csv(airlinePolarity, file.path("~", "Github","ieseDataSciTwitterProject", "airlinePolarityAfinn.csv", fsep = "/"))
+
+
+
